74f85fce7024813ec275ca4e9170ad9ec1f1f779
Paul Van den Bergh <VdBergh.Paul@gmail.com>
2017-10-25 16:23:29 +0200

    Working version

--- BBB_PRU/main.c
+++ BBB_PRU/main.c
@@ -16,8 +16,9 @@
 
 #include "resource_table.h"
 
-#define RAILCOM_BITPOS 0
-#define DCC_BITPOS 1
+#define DCC_BITPOS 0
+#define RAILPOWER_BITPOS 1
+#define RAILCOMGAP_BITPOS 2
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
@@ -37,14 +38,14 @@ void SendBit(const uint8_t bit)
     {
         __R30 |= (1 << DCC_BITPOS);
         __delay_cycles(20000);
-        __R30 &= !(1 << DCC_BITPOS);
+        __R30 &= ~(1 << DCC_BITPOS);
         __delay_cycles(20000);
     }
     else
     {
         __R30 |= (1 << DCC_BITPOS);
         __delay_cycles(11600);
-        __R30 &= !(1 << DCC_BITPOS);
+        __R30 &= ~(1 << DCC_BITPOS);
         __delay_cycles(11600);
     }
 }
@@ -66,20 +67,28 @@ inline
 void RailComCutout(void)
 {
     __R30 |= (1 << DCC_BITPOS);
-    __delay_cycles(5800);   //  Tcs
-    __R30 |= (1 << RAILCOM_BITPOS);
-    __delay_cycles(47100);
-    __R30 &= !(1 << DCC_BITPOS);
-    __delay_cycles(47100);
-    __R30 &= !(1 << RAILCOM_BITPOS);
-    __delay_cycles(10);
+    __delay_cycles(4800);
+    __R30 &= ~(1 << RAILPOWER_BITPOS);
+    __delay_cycles(900);
+    __R30 |= (1 << RAILCOMGAP_BITPOS);
+    __delay_cycles(900);
+    __R30 |= (1 << RAILPOWER_BITPOS);
+    __delay_cycles(86600);
+    __R30 &= ~(1 << RAILPOWER_BITPOS);
+    __delay_cycles(900);
+    __R30 &= ~(1 << RAILCOMGAP_BITPOS);
+    __delay_cycles(900);
+    __R30 |= (1 << RAILPOWER_BITPOS);
+    __delay_cycles(9400);
+    __R30 &= ~(1 << DCC_BITPOS);
+    __delay_cycles(11600);
 }
 
 void SendCommand(const uint8_t* pCmd)
 {
     uint8_t i;
 
-    for(i = 0; i < 16; i++)
+    for(i = 0; i < 12; i++)
     {
         SendBit(1);
     }
@@ -91,16 +100,16 @@ void SendCommand(const uint8_t* pCmd)
 
     SendBit(1);
 
-//    RailComCutout();
+    RailComCutout();
 }
 
 
 int main(void)
 {
+#ifdef PRU0
     static const uint8_t ResetCmd[] = { 0x04, 0x00, 0x00, 0x00 };
     static const uint8_t IdleCmd[] = { 0x04, 0xFF, 0x00, 0xFF };
-
-    __R30 &= 0xFFFFFF00;
+    static const uint8_t SpeedCmd[] = { 0x04, 0x03, 0x4F, 0x4C};
 
     struct pru_rpmsg_transport transport;
     uint16_t src, dst, len;
@@ -129,21 +138,21 @@ int main(void)
         /* Check bit 30 of register R31 to see if the ARM has kicked us */
         if (__R31 & HOST_INT)
         {
-            __R30 |= 0x08;
-            /* Clear the event status */
-            CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
             /* Receive all available messages, multiple messages can be sent per kick */
             while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS)
             {
-                /* Echo the message back to the same address from which we just received */
-                pru_rpmsg_send(&transport, dst, src, payload, len);
-                SendCommand(ResetCmd);
+                SendCommand(payload);
             }
-            __R30 &= ~(0x08);
+
+            /* Clear the event status */
+            CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
         }
         else
         {
             SendCommand(IdleCmd);
         }
     }
+#else
+    __halt();
+#endif
 }
